0334a560e20f7f2826f476580fe821ee32232c35
erickulchycki <erickulchycki@gmail.com>
2020-02-16 15:24:37 -0600

    Hmmmm

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def hello_world():
-    print(request)
-    return "hey"
+    print(request.body.token)
+    return {
+        'token': os.environ.get('TOKEN'),
+        'challenge': request.body.token,
+        'type': 'url_verification'
+    }
 
 if __name__  == '__main__':
     app.run(host='0.0.0.0')
