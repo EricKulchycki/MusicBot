b58552482dd07a75e2fbc2d831b6544fef1d6b93
Eric Kulchycki <erickulchycki@gmail.com>
2020-02-17 23:27:41 +0000

    Got a response baby

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
-def hello_world():
-    print(request.body.token)
+def init_app():
     return {
-        'token': os.environ.get('TOKEN'),
-        'challenge': request.body.token,
+        'token': "Yo8dkYH5HLme7rjo2DtB7mNl",
+        'challenge': request.json['challenge'],
         'type': 'url_verification'
     }
+
 
 if __name__  == '__main__':
     app.run(host='0.0.0.0')
