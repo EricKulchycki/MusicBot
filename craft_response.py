85eb80ba2b29cb0ea560cedd77a0f5a9afca280a
erickulchycki <erickulchycki@gmail.com>
2020-02-17 21:39:06 -0600

    Get album links

--- craft_response.py
+++ craft_response.py
@@ -6,9 +6,20 @@ class CraftResponse:
         self.response = ""
 
     def get_links(self, data):
-        print(data)
         fields = data['data']
         artist = fields['artists'][0]
         album_name = fields['name']
-        self.response += "The Linked Album is {} by {}".format(artist['name'], album_name)
+        links = fields['links']
+        spotify = links['spotify'][0]['link']
+        googleplay = links['googleplay'][0]['link']
+        applemusic = self.format_apple_link(links['itunes'][0]['link'], "ca")
+        self.response += "The Linked Album is {} by {}\n".format(artist['name'], album_name)
+        self.response += "Spotify Link: {}\n".format(spotify)
+        self.response += "Google Play Link: {}\n".format(googleplay)
+        self.response += "Apple Music Link: {}\n".format(applemusic)
         return self.response
+
+    def format_apple_link(self, link, country):
+        pre = link.split("{")[0]
+        post = link.split("}")[1]
+        return pre + country + post
