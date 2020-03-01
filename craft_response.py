bc112a4151ce19820ed43ed862ad8bff4f61cdff
Ubuntu <ubuntu@ip-172-16-11-35.ec2.internal>
2020-03-01 16:45:50 +0000

    Kinda works sometimes

--- craft_response.py
+++ craft_response.py
@@ -6,13 +6,21 @@ class CraftResponse:
         self.response = ""
 
     def get_links(self, data):
+        self.response = ""
         fields = data['data']
         artist = fields['artists'][0]
         album_name = fields['name']
         links = fields['links']
-        spotify = links['spotify'][0]['link']
-        googleplay = links['googleplay'][0]['link']
-        applemusic = self.format_apple_link(links['itunes'][0]['link'], "ca")
+        spotify = ''
+        googleplay = ''
+        applemusic = ''
+        
+        if 'spotify' in links:
+            spotify = links['spotify'][0]['link']
+        if 'googleplay' in links:
+            googleplay = links['googleplay'][0]['link']
+        if 'itunes' in links:
+            applemusic = self.format_apple_link(links['itunes'][0]['link'], "ca")
         self.response += "The Linked Album is {} by {}\n".format(album_name, artist['name'])
         self.response += "Spotify Link: {}\n".format(spotify)
         self.response += "Google Play Link: {}\n".format(googleplay)
